8fc998da3df85ba2182e854ac523b2d45e07fab3
leowu0329 <ryowuandjanet@gmail.com>
2025-04-16 23:57:53 +0800

    54:46 - Section 2 - Setting Up Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import Navbar from './components/navbar.component';
-
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signup" element={<h1>Sign Up page</h1>} />
+        <Route path="signin" element={<h1>Sign In page</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
