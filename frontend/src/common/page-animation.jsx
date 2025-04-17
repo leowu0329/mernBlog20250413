96b3638860234835d59f3d3dff35e48031448d2a
leowu0329 <ryowuandjanet@gmail.com>
2025-04-17 23:03:15 +0800

    01:37:48 - Section 4 - Page Animation

---
+++ frontend/src/common/page-animation.jsx
@@ -0,0 +1,27 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
+const AnimationWrapper = ({
+  children,
+  keyValue,
+  initial = { opacity: 0 },
+  animate = { opacity: 1 },
+  transition = { duration: 1 },
+  className,
+}) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={keyValue}
+        initial={initial}
+        animate={animate}
+        exit={{ opacity: 0 }}
+        transition={transition}
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default AnimationWrapper;
