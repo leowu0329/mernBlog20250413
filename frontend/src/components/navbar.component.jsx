a28e3e861a8db2b93fda61023048bc7d3db40080
leowu0329 <ryowuandjanet@gmail.com>
2025-04-17 22:50:07 +0800

    01:02:08 - Section 3 - User Auth. Form ( UI )

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
         </Link>
         <div
           className={
-            'absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block-0 md:block md:relative md:inset-0 md:p-0 md:w-auto' +
-            (searchBoxVisibility ? 'show' : 'hide')
+            'absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:relative md:inset-0 md:p-0 md:w-auto md:show' +
+            (searchBoxVisibility ? ' show' : ' hide')
           }
         >
           <input
@@ -26,9 +26,7 @@ const Navbar = () => {
         <div className="flex items-center gap-3 md:gap-6 ml-auto">
           <button
             className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
-            onClick={() =>
-              setSearchBoxVisibility((currentValue) => !currentValue)
-            }
+            onClick={() => setSearchBoxVisibility((currentVal) => !currentVal)}
           >
             <i className="fi fi-rr-search text-xl"></i>
           </button>
